f11134c7d606b0b7c5fc95d91cf696364ccad9af
KH <96801545+kleineshertz@users.noreply.github.com>
2022-11-18 20:49:30 -0800

    Fixed ca_path for dockerized daemon (#10)
    * Fixed ca_path for dockerized daemon
    
    Co-authored-by: KH <>

--- pkg/exe/daemon/docker/docker-startup.sh
+++ pkg/exe/daemon/docker/docker-startup.sh
@@ -2,6 +2,7 @@
 
 cd /usr/local/bin
 sed -i -e 's~"url":[ ]*"[a-zA-Z0-9@\.:\/\-_$ ]*"~"url": "'"$AMQP_URL"'"~g' env_config.json
-sed -i -e 's~"hosts\":[ ]*\[[0-9a-zA-Z\.\,\-_ "]*\]~"hosts": '"$CASSANDRA_HOSTS~g" env_config.json
+sed -i -e 's~"hosts":[ ]*\[[0-9a-zA-Z\.\,\-_ "]*\]~"hosts": '"$CASSANDRA_HOSTS~g" env_config.json
+sed -i -e 's~"ca_path":[ ]*\"[0-9a-zA-Z\.\,\-_\/ ]*\"~"ca_path": "/usr/src/capillaries/test/ca"~g' env_config.json
 cat  env_config.json
 daemon
