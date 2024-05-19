2348af34f41d7c5c683256244df8f8fc6e8b8a32
KH <>
2024-05-18 18:51:05 -0700

    fluentd working

--- copy_demo_data.sh
+++ copy_demo_data.sh
@@ -1,10 +1,17 @@
-rm -fR /tmp/capi_cfg
-rm -fR /tmp/capi_in
-rm -fR /tmp/capi_out
+sudo rm -fR /tmp/capi_cfg
+sudo rm -fR /tmp/capi_in
+sudo rm -fR /tmp/capi_out
+sudo rm -fR /tmp/capi_log
 
 mkdir /tmp/capi_cfg
 mkdir /tmp/capi_in
 mkdir /tmp/capi_out
+mkdir /tmp/capi_log
+
+chmod 777 /tmp/capi_cfg
+chmod 777 /tmp/capi_in
+chmod 777 /tmp/capi_out
+chmod 777 /tmp/capi_log
 
 cp -r ./test/data/cfg/* /tmp/capi_cfg
 cp -r ./test/data/in/* /tmp/capi_in
