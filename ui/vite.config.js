ca12dac639aacfa9b6630f7fe79c6bc3a413607d
KH <>
2024-05-25 14:02:34 -0700

    vite config cleanup

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -5,26 +5,7 @@ console.log("In .env file VITE_WEBAPI_URL=$CAPI_WEBAPI_URL:", process.env.CAPI_W
 export default defineConfig({
 	server: {
 		port: 8080,
-		host: true,   // In Docker containers, "--host 0.0.0.0" may not work. So, tell Vite to bind to 0.0.0.0 here.
-		proxy: {
-			'/': {
-			  target: 'https://localhost:44305',
-			  changeOrigin: true,
-			  secure: false,      
-			  ws: true,
-			  configure: (proxy, _options) => {
-				proxy.on('error', (err, _req, _res) => {
-				  console.log('proxy error', err);
-				});
-				proxy.on('proxyReq', (proxyReq, req, _res) => {
-				  console.log('Sending Request to the Target:', req.method, req.url);
-				});
-				proxy.on('proxyRes', (proxyRes, req, _res) => {
-				  console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-				});
-			  },
-			}
-		  }
+		host: true   // In Docker containers, "--host 0.0.0.0" may not work. So, tell Vite to bind to 0.0.0.0 here.
 	},
 	plugins: [
 		sveltekit()
