0b21e5f971c80af52c7608e13bd32b14f358fefd
KH <>
2024-09-22 11:05:29 -0700

    Test integration comments

--- test_integration.sh
+++ test_integration.sh
@@ -38,14 +38,20 @@ popd
 
 # s3
 
-# These will require something like
-# export CAPILLARIES_AWS_TESTBUCKET=capillaries-testbucket
+# The section below requires 3 things.
+
+# 1. Capillaries binaries (daemon and webapi) need AWS credentials to access S3 artifacts 
 # export AWS_ACCESS_KEY_ID=...
 # export AWS_SECRET_ACCESS_KEY=...
 # export AWS_DEFAULT_REGION=us-east-1
-# for webapi and daemon. Make sure you set those either:
-# - before running docker containers with webapi and daemon
+# Make sure you set those either:
+# - before running docker containers with webapi and daemon, so docker-compose.yml will pass those variables to the binaries
 # - before executing webapi/daemon from the cmd line or from the debugger 
+
+# 2. The commands below need same AWS credentials to access S3 artifacts, so set those variables before running this script.
+
+# 3. The commands below need to know the location of S3 artifacts, so, before running this script, specify something like
+# export CAPILLARIES_AWS_TESTBUCKET=capillaries-testbucket
 
 pushd ./test/code/lookup/quicktest_s3
 ./test_one_run_local.sh
