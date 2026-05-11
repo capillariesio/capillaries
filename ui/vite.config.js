df6234cd54c009387a91b34d9c6d3d59ce11ce23
KH <>
2026-05-10 19:18:38 -0700

    Version in ui

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { execSync } from 'child_process'
+
+const gitHash = execSync('git describe --tags --always').toString().trim();
 
 console.log('In .env file VITE_WEBAPI_URL=$CAPI_WEBAPI_URL:', process.env.CAPI_WEBAPI_URL);
 export default defineConfig({
+	define: {
+        __GIT_HASH__: JSON.stringify(gitHash),
+    },
 	server: {
 		port: 8080,
 		host: true // In Docker containers, "--host 0.0.0.0" may not work. So, tell Vite to bind to 0.0.0.0 here.
